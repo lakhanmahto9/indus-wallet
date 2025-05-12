b95f2d4ca651ce0284160459f91c2079d14637b9
lakhan <lakhanmahto3676@gmail.com>
2025-05-12 23:26:34 +0530

    green

--- src/components/home/Usdtpage.jsx
+++ src/components/home/Usdtpage.jsx
@@ -5,7 +5,7 @@ const Usdtpage = () => {
   return (
     <div className="w-full flex justify-center items-center flex-col p-4 sm:p-8">
       <div className="flex gap-2 justify-center items-center">
-        <p className="text-3xl font-bold">67.89 BNB</p>
+        <p className="text-3xl font-bold text-green-500">67.89 BNB</p>
         <EyeIcon color="" width="18" height="18" />
       </div>
       <p className="text-lg font-medium">
